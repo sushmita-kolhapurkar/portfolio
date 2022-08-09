a607b3891d27263c6227d751cb3a8ac2d7d5cdb9
Sushmita Kolhapurkar <61219016+sushmita-kolhapurkar@users.noreply.github.com>
2022-08-09 18:25:40 -0400

    Update parallax.js

--- assets/parallax.js
+++ assets/parallax.js
@@ -1,17 +1,23 @@
 // constant elements: your main scrolling element; html element
 const scrollEl = document.documentElement
 const root = document.documentElement
+const rollflower = document.querySelector('.rollflower');
 
-var scrollPos
+var scrollPos, rollPos;
 
 // update css property on scroll
 function animation() {
   // check the scroll position has changed
   if (scrollPos !== scrollEl.scrollTop) {
+	  rollflower.classList.add('rollfloweranim');
+
     // reset the seen scroll position
     scrollPos = scrollEl.scrollTop
     // update css property --scrollPos with scroll position in pixels
     root.style.setProperty('--scrollPos', scrollPos + 'px')
+    root.style.setProperty('--rollPos', rollPos + 'deg')
+    root.style.setProperty('--radPos', scrollPos/100 + 'rad')
+	
   }
 
   // call animation again on next animation frame
